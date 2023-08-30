d1e97f0bfbebc57a662e8e56848d385f5b4755fa
johncovenant <ufotjohnamos@gmail.com>
2023-08-30 12:39:49 +0100

    fixed styles import error

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,8 @@
+import React, { useEffect, UseState } from 'react';
+import { Link } from 'react-router-dom'
+
+import { styles } from '../styles';
+import {logo, menu, close} from '../assets';
 
 
 const Navbar = () => {
