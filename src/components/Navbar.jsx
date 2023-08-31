dc71ea0b51c538c96860182cd0c7fb51bbe560e7
johncovenant <ufotjohnamos@gmail.com>
2023-08-31 17:34:43 +0100

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, UseState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import { styles } from '../styles';
@@ -6,10 +6,22 @@ import {logo, menu, close} from '../assets';
 
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-
+        <Link
+          to="/" 
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+          >
+            <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+            <p className='text-white text-[18px]font-bold cursor-pointer'>John <span className='sm:block hidden'>Covenant</span></p>
+        </Link>
       </div>
     </nav>
   )
