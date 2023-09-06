c4c5d30e8f433b684bfe6d23bc39c53bd1bba0d3
johncovenant <ufotjohnamos@gmail.com>
2023-09-06 16:05:23 +0100

    added framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
         <div className='flex flex-col md:flex-row items-center'>
           <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff] sm:block'>John Covenant</span></h1>
+            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I&apos;m <span className='text-[#915eff] sm:block'>John Covenant</span></h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop web2/web3 applications,<br></br> user interfaces and Data Analysis</p>
           </div>
           <img src={cj} alt="billing" className='w-40 h-40 mx-auto md:w-[30%] md:h-[40%] relative flex flex-col items-end md:ml-5' />
@@ -25,7 +25,19 @@ const Hero = () => {
       {/* button that moves users to the bottom of the contact section of the page */}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#contact">
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'></div>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev 
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
         </a>
       </div>
     </section>
