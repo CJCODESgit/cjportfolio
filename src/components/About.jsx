d43dc0a69825e245d5cb738e5825b864e7354818
johncovenant <ufotjohnamos@gmail.com>
2023-09-06 16:30:51 +0100

    working on about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,8 @@
-
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from '../utils/motion';
 
 const About = () => {
   return (
