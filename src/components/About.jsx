82a695d75f0eea9517c0a32b6a43106d6c8b76a0
johncovenant <ufotjohnamos@gmail.com>
2023-09-07 17:22:12 +0100

    included about me and overview

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,16 @@ const About = () => {
       <h2 className={styles.sectionHeadText}>Overview.</h2>
     </motion.div>
 
-      <motion.p>
-      
+      <motion.p
+        variants={fadeIn("","", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I&apos;m a skilled and passionate web developer with experience in Javascript, React, Python, jupyter and
+        expertise in web3, machine learning, data analysis, the
+        Microsoft Office suite, and social media management, as
+        well as a talent for data analysis and proficiency in the
+        Microsoft Office suite.
+        I&apos;m motivated, dedicated, a quick learner and collaborate closely with clients to create solutions and eager to bring my skills and passion to a new role.
     </motion.p>
     </>
   )
