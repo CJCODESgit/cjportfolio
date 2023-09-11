0e5a7cab0501749ceb907cba3ca756e2763424f3
johncovenant <ufotjohnamos@gmail.com>
2023-09-11 17:57:39 +0100

    applying tech images

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,12 @@
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
 
+// eslint-disable-next-line react-refresh/only-export-components
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28 xs:w-14 xs:h-14" key={technology.name}>
+        <div className="w-14 h-14 sm:w-28 sm:h-28" key={technology.name}>
           <img src={technology.icon} />
         </div>
 
--- src/constants/index.js
+++ src/constants/index.js
@@ -8,13 +8,13 @@ import {
   html,
   css,
   reactjs,
-  redux,
+  // redux,
   tailwind,
   nodejs,
-  mongodb,
+  // mongodb,
   git,
-  figma,
-  docker,
+  // figma,
+  // docker,
   meta,
   starbucks,
   tesla,
@@ -22,7 +22,7 @@ import {
   carrent,
   jobit,
   tripguide,
-  threejs,
+  // threejs,
 } from "../assets";
 
 export const navLinks = [
@@ -60,6 +60,7 @@ const services = [
 ];
 
 const technologies = [
+  //commented tech are those i'm still learning
   {
     name: "HTML 5",
     icon: html,
@@ -80,10 +81,10 @@ const technologies = [
     name: "React JS",
     icon: reactjs,
   },
-  {
-    name: "Redux Toolkit",
-    icon: redux,
-  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
   {
     name: "Tailwind CSS",
     icon: tailwind,
@@ -92,26 +93,26 @@ const technologies = [
     name: "Node JS",
     icon: nodejs,
   },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
+  // {
+  //   name: "MongoDB",
+  //   icon: mongodb,
+  // },
+  // {
+  //   name: "Three JS",
+  //   icon: threejs,
+  // },
   {
     name: "git",
     icon: git,
   },
-  {
-    name: "figma",
-    icon: figma,
-  },
-  {
-    name: "docker",
-    icon: docker,
-  },
+  // {
+  //   name: "figma",
+  //   icon: figma,
+  // },
+  // {
+  //   name: "docker",
+  //   icon: docker,
+  // },
 ];
 
 const experiences = [
