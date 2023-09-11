3cb641a5701c9b601b7663967e444049527a02dc
johncovenant <ufotjohnamos@gmail.com>
2023-09-11 18:14:54 +0100

    fixed skills animation

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,29 @@
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from '../utils/motion';
 
 // eslint-disable-next-line react-refresh/only-export-components
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    
+    <div>
+      <motion.div variants={textVariant()}>
+      <h2 className={styles.sectionHeadText}>My Skills</h2>
+    </motion.div>
+
+    <motion.div 
+      variants={fadeIn("right", "spring", 0.5 )}
+      className="flex flex-row flex-wrap justify-center gap-10"
+    >
       {technologies.map((technology) => (
-        <div className="w-14 h-14 sm:w-28 sm:h-28" key={technology.name}>
+        <div className="mt-4 w-14 h-14 sm:w-28 sm:h-28" key={technology.name}>
           <img src={technology.icon} />
         </div>
 
       ))}
+      </motion.div>
     </div>
   )
 }
