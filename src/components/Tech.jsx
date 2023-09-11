7bec148bf6687deadab1fc7c8a592b29f4294f35
johncovenant <ufotjohnamos@gmail.com>
2023-09-11 17:47:21 +0100

    fixed issue with app.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constants"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28 xs:w-14 xs:h-14" key={technology.name}>
+          <img src={technology.icon} />
+        </div>
+
+      ))}
+    </div>
   )
 }
 
-export default Tech
+// eslint-disable-next-line react-refresh/only-export-components
+export default SectionWrapper(Tech, "")
