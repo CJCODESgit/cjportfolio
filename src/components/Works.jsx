3a49530b437b61e7e5e666a7b777abefec6b9fe7
johncovenant <ufotjohnamos@gmail.com>
2023-09-15 18:52:35 +0100

    expense tracker project image

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -11,8 +11,13 @@ import { fadeIn, textVariant } from '../utils/motion';
 const Works = () => {
   return (
     <>
-      <motion.div>
-        
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          My work
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Projects.
+        </h2>
       </motion.div>
     </>
   )
