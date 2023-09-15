2c955bb1acc4981b652067a9e8807af5f5815782
johncovenant <ufotjohnamos@gmail.com>
2023-09-15 23:36:28 +0100

    saving progress

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,9 +15,28 @@ const ProjectCard = ({index, name, description, tags, image, source_code_link}) 
           scale: 1,
           speed: 450
         }}
-        className
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div 
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+        
       </Tilt>
     </motion.div>
   )
@@ -41,10 +60,11 @@ const Works = () => {
           className="mt-3 text-secondary text-[17px]
           max-w-3xl leading-[30px]" 
         >
-          Projects displayed below showcases my skills and my experience. It shows my ability to solve problems and work with different technologies. contains links to repositories and demos in it.
+          Projects displayed below showcases my skills and my experience. It shows my ability to solve problems and work with different technologies. contains links to repositories and demos in it. click on the github logos to visit the github repository for each project.
         </motion.p>
 
-        <div className="mt-20 flex flex-wrap gap-7">
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
           {projects.map((project, index) => (
             <ProjectCard
               key={`project-${index}`}
@@ -53,7 +73,6 @@ const Works = () => {
             />
           ))}
         </div>
-      </div>
     </>
   )
 }
