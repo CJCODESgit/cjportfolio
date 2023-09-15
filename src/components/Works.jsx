c72ae1ce78e8316e6539933041f39a1911a1e009
johncovenant <ufotjohnamos@gmail.com>
2023-09-15 14:46:23 +0100

    fixed react-tilt import

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import Tilt from 'react-tilt';
+import { Tilt } from "react-tilt";
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { github } from '../assets';
@@ -7,14 +7,14 @@ import { projects } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion';
 
 
-// eslint-disable-next-line react-refresh/only-export-components
+
 const Works = () => {
   return (
-    <>
-    
-    </>
+    <div>
+
+    </div>
   )
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
-export default SectionWrapper(Works, "");
+export default SectionWrapper(Works, "")
